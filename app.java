354fc06ce506a074b7af6d8fe489f28fca23c83f
Vladimir Scacodub <newhalf0@gmail.com>
2023-09-24 17:02:59 +0300

    commit new branch new api

--- app.java
+++ app.java
@@ -1,5 +1,6 @@
 class app {
 public static void main (String [] args){
 	System.out.println("Hello world!");
-	System.out.println("Hello Git!");	
+	System.out.println("Hello Git!");
+	System.out.println("Hello from NEW api brench!")
 }
