f0e8bfbef08c622a1750a86a993606e60872e239
Vladimir Scacodub <newhalf0@gmail.com>
2023-09-24 17:50:19 +0300

    Commit in alebranch

--- app.java
+++ app.java
@@ -1,5 +1,7 @@
 class app {
 public static void main (String [] args){
 	System.out.println("Hello world!");
-	System.out.println("Hello Git!");	
+	System.out.println("Hello Git!");
+	System.out.println("Hello form alebranch branch!");
+}
 }
