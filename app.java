fd319985338fe31d8e9a2c50265e62d859b19972
Vladimir Scacodub <newhalf0@gmail.com>
2023-09-24 17:07:16 +0300

    coomit changes 1212

--- app.java
+++ app.java
@@ -2,5 +2,6 @@ class app {
 public static void main (String [] args){
 	System.out.println("Hello world!");
 	System.out.println("Hello Git!");
-	System.out.println("Hello from NEW api brench!")
+	System.out.println("Hello from NEW api brench!");
+}
 }
