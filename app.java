07b990a8276eb5949a6340e2eefaadbfaf9dd24b
Scacodub Vladimir <Vladimir.Scacodub@endava.com>
2023-09-25 12:01:37 +0300

    Creating differences for Another Branch

--- app.java
+++ app.java
@@ -13,5 +13,6 @@ public static void main (String [] args){
 	System.out.println("Hello from the second employee!");
 	System.out.println("Hello from ANOThER COMPUTER");
 	System.out.println("Hello from another branch!");
+	System.out.println("Hello from ANOTHER Branch!");
 }
 }
