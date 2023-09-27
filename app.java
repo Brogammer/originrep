d5aab477f8450697f0f33903402911440455b617
Scacodub Vladimir <Vladimir.Scacodub@endava.com>
2023-09-27 12:14:58 +0300

    Creating changes for cherry picking 2

--- app.java
+++ app.java
@@ -18,6 +18,8 @@ public static void main (String [] args){
 	System.out.println("Hello from ANOTHER Branch");
 	System.out.println("Hello my second changes!");
 
-	System.out.println("Hello from cherry pick");
+	System.out.println("Hello from cherry pick branch");
+	System.out.println("Making some changes for cherry picking!");
+
 }
 }
