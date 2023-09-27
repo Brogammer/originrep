80d2a776afa9166039e75a6117463cf2c30e52d3
Scacodub Vladimir <Vladimir.Scacodub@endava.com>
2023-09-27 12:42:57 +0300

    I cherry-picked some commit from cherrypick branch

--- app.java
+++ app.java
@@ -28,6 +28,9 @@ public static void main (String [] args){
 	for (int i = 0; i < 10; i++){
 		System.out.println("Changes in master branch");
 	}
+	for (int i = 0; i < 10; i++){
+		System.out.println("Cherry pick");
+	}
 
 
 }
